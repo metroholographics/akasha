604ab75b276b7ebe5dbcb66aee76d99619cb78e0
ali raja <nirvaanraja@gmail.com>
2025-09-07 21:17:57 +0100

    Resizeable window, resolution adapting (unsure if properly working) and sprite loading

--- src/main.c
+++ src/main.c
@@ -12,25 +12,47 @@ GameState game;
 int main (int argc, char *argv[])
 {
     (void)argc; (void)argv;
-    InitWindow(G_WIDTH, G_HEIGHT, "akasha");
+    game.dimensions.window_width  = G_DEFAULT_WIDTH;
+    game.dimensions.window_height = G_DEFAULT_HEIGHT;
+    game.dimensions.screen_width  = RENDER_W;
+    game.dimensions.screen_height = RENDER_H;
 
-    game.screen = LoadRenderTexture(WIDTH, HEIGHT);
+    InitWindow(game.dimensions.window_width, game.dimensions.window_height, "akasha");
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+    game.screen = LoadRenderTexture(RENDER_W, RENDER_H);
     SetTextureFilter(game.screen.texture, TEXTURE_FILTER_POINT);
 
-    SetTargetFPS(60);
+    game.spritesheet = LoadTexture("assets/spritesheet.png");
+
+    SetTargetFPS(120);
     while (!WindowShouldClose()) {
-    //Drawing to 480x320 render texture
+
+        {
+            game.dimensions.window_width  = GetScreenWidth();
+            game.dimensions.window_height = GetScreenHeight();
+        }
+        float scale = fminf((float)game.dimensions.window_width / game.dimensions.screen_width,
+            (float)game.dimensions.window_height / game.dimensions.screen_height);
+        float screen_w = game.dimensions.screen_width * scale;
+        float screen_h = game.dimensions.screen_height * scale;
+
+        Vector2 screen_pos = get_centered_top_left(screen_w, screen_h, game.dimensions.window_width, game.dimensions.window_height);
+        
+        //Drawing to render texture
         BeginTextureMode(game.screen);
-            ClearBackground(BLACK);
+            ClearBackground(DARKBROWN);
+            DrawTexturePro(game.spritesheet, (Rectangle){0,0,32,32}, (Rectangle){100,100,32,32}, (Vector2){0,0}, 0.0f, WHITE);
+            
         EndTextureMode();
 
         //Drawing to frame buffer
         BeginDrawing();
-            ClearBackground(WHITE);
+            ClearBackground(BLACK);
             DrawTexturePro(
                 game.screen.texture,
-                (Rectangle){0, 0, WIDTH, -HEIGHT},
-                (Rectangle){0, 0, G_WIDTH, G_HEIGHT},
+                (Rectangle){0, 0, RENDER_W, -RENDER_H},
+                (Rectangle){screen_pos.x, screen_pos.y, screen_w, screen_h},
                 (Vector2){0,0},
                 0.0f,
                 WHITE
@@ -40,4 +62,15 @@ int main (int argc, char *argv[])
 
     UnloadRenderTexture(game.screen);
     CloseWindow();
+}
+
+Vector2 get_centered_top_left(float w, float h, float box_w, float box_h)
+{
+    float cen_x = box_w / 2;
+    float cen_y = box_h / 2;
+
+    float c_x = cen_x - (0.5f * w);
+    float c_y = cen_y - (0.5f * h);
+
+    return (Vector2){c_x, c_y};
 }
--- src/main.h
+++ src/main.h
@@ -1,14 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define WIDTH 480
-#define HEIGHT 320
-#define G_WIDTH 1440//(WIDTH * SCALE)
-#define G_HEIGHT 960//(HEIGHT * SCALE)
+#define ASPECT_W 16
+#define ASPECT_H 9
+#define RENDER_W 1280
+#define RENDER_H 960
+#define G_DEFAULT_WIDTH 960
+#define G_DEFAULT_HEIGHT 640
+
+typedef struct dimensions {
+    int window_width;
+    int window_height;
+    int screen_width;
+    int screen_height;
+} Dimensions;
 
 typedef struct game_state {
     RenderTexture2D screen;
+    Texture2D spritesheet;
+    Dimensions dimensions;
 } GameState;
+
+Vector2 get_centered_top_left(float w, float h, float box_w, float box_h);
 
 
 #endif
